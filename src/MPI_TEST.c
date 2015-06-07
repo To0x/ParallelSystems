a8f493eb7e6f4a07411464b2fca17fd86cc02ba5
LittleShorty <s0535422@htw-berlin.de>
2015-06-07 22:04:39 +0200

    Implemented keyword counter.

--- src/MPI_TEST.c
+++ src/MPI_TEST.c
@@ -16,20 +16,23 @@
 #include <wchar.h>
 #include "dataHolder.h"
 
-int main(int argc, char* argv[]){
-	int  my_rank; /* rank of process */
-	int  p;       /* number of processes */
-	int source;   /* rank of sender */
-	int dest;     /* rank of receiver */
-	int tag=0;    /* tag for messages */
-	char message[100];        /* storage for message */
-	MPI_Status status ;   /* return status for receive */
-	
+int main(int argc, char* argv[]) {
+	int my_rank; /* rank of process */
+	int p; /* number of processes */
+	int source; /* rank of sender */
+	int dest; /* rank of receiver */
+	int tag = 0; /* tag for messages */
+	char message[100]; /* storage for message */
+	MPI_Status status; /* return status for receive */
+
 	// MPI Initializations
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
-	
+
+	// Show keyword
+	printf("Tweets will be sorted by keyword '%s'.\n", argv[1]);
+
 	// File Initializations
 	FILE *fp;
 	fp = fopen("./65536tweets.0", "rb+, ccs=UTF-8");
@@ -39,15 +42,13 @@ int main(int argc, char* argv[]){
 	}
 
 	int errorCode = 0;
-	if (my_rank == 0){
+	if (my_rank == 0) {
 		/* create message */
 		sprintf(message, "Hello MPI World from process %d!", my_rank);
 		dest = 0;
 
-
 		//byte *test = readTweets("./");
 		//////////
-
 
 		//int i = 0;
 		unsigned char c;
@@ -55,47 +56,52 @@ int main(int argc, char* argv[]){
 
 		struct tweetData *td;
 
+		int i = 0;
 		while (!feof(fp)) {
 			unsigned char *line = readLine(fp);
 
+			if (i == 5) {
+				break;
+			}
+			i++;
+
 			/*
-			while ((c = *line) != '\0') {
-				//actInt = (uint16_t) c;
+			 while ((c = *line) != '\0') {
+			 //actInt = (uint16_t) c;
 
-		        actInt = (unsigned char)c;
+			 actInt = (unsigned char)c;
 
-		        printf("%c - ", c);
-		        printf("%u", actInt);
+			 printf("%c - ", c);
+			 printf("%u", actInt);
 
-		        printf("\n");
+			 printf("\n");
 
-				line++;
-			}*/
+			 line++;
+			 }*/
 
+			td = parseTweet(line, argv[1]);
 
-			td = parseTweet(line, "test");
-
-			printf("%s - Hashtags: %d, Smileys: %d\n", line, td->hashtags, td->smiles);
-
+			printf("%s - Hashtags: %d, Smileys: %d, Keywords: %d\n", line, td->hashtags,
+					td->smiles, td->keywords);
 
 			//i++;
 			//if (i==5)
 			//	break;
 
 			/*
-			c = *line;
-			while (c != 0) {
-				wprintf(L"%lc-", c);
-				c = *line++;
-			}
+			 c = *line;
+			 while (c != 0) {
+			 wprintf(L"%lc-", c);
+			 c = *line++;
+			 }
 
-			i++;
-			printf("\n");
-			//printf("%c", line);
+			 i++;
+			 printf("\n");
+			 //printf("%c", line);
 
-			if (i == 5)
-				break;
-			*/
+			 if (i == 5)
+			 break;
+			 */
 		}
 
 		MPI_Finalize();
@@ -105,18 +111,16 @@ int main(int argc, char* argv[]){
 		//MPI_Send(message, strlen(message)+1, MPI_CHAR,
 		//   dest, tag, MPI_COMM_WORLD);
 
-	}
-	else{
-		printf("Hello MPI World From process 0: Num processes: %d\n",p);
+	} else {
+		printf("Hello MPI World From process 0: Num processes: %d\n", p);
 		for (source = 1; source < p; source++) {
-			MPI_Recv(message, 100, MPI_CHAR, source, tag,
-			      MPI_COMM_WORLD, &status);
-			printf("%s\n",message);
+			MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD,
+					&status);
+			printf("%s\n", message);
 		}
 	}
 	/* shut down MPI */
-	MPI_Finalize(); 
-	
-	
+	MPI_Finalize();
+
 	return 0;
 }
--- src/dataHolder.c
+++ src/dataHolder.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "dataHolder.h"
 
 //#define PARSE(__x__, __y__) y=((unsigned char)*(++__x__);)
@@ -18,10 +20,25 @@ void resetTweetData(struct tweetData *td) {
 }
 
 unsigned char next(unsigned char *tweet) {
-	unsigned char retVal = (unsigned char)*(++tweet);
+	unsigned char retVal = (unsigned char) *(++tweet);
 	printf("%c %d\n", retVal, retVal);
 	return retVal;
 	//return (unsigned char)*(++tweet);
+}
+
+bool isKeyWord(unsigned char* tweetString, char* keyword) {
+	// To avoid to build a for loop for every letter
+	if (tweetString[0] != keyword[0]) {
+		return false;
+	}
+
+	for (int i = 1; i < strlen(keyword); i++) {
+		if (tweetString[i] < 128 && tweetString[i] != keyword[i]) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 struct tweetData* parseTweet(unsigned char* tweetString, char* keyWord) {
@@ -35,52 +52,52 @@ struct tweetData* parseTweet(unsigned char* tweetString, char* keyWord) {
 
 	while ((actChar = *tweetString) != '\0') {
 
-        if (actChar < 128) {// 0xxx xxxx --> befindet sich im ASCII bereich
-        	actCharByteLenght = 1;
+		if (actChar < 128) { // 0xxx xxxx --> befindet sich im ASCII bereich
+			actCharByteLenght = 1;
 
-        	if (actChar == '#')
-        		thisData->hashtags++;
-        }
-        else if (actChar < 224) { // 110x xxxx --> UTF8 Encoding beginnt 2Byte
-        	actCharByteLenght = 2;
-        	tweetString++;
-        }
-        else if (actChar < 240) {// 1110 xxxx --> 3 Byte
-        	actCharByteLenght = 3;
-        	tweetString+=2;
-        }
-        else {
-        	actCharByteLenght = 4; // 1111 0xxx --> 4 Byte
+			if (isKeyWord(tweetString, keyWord)) {
+				thisData->keywords++;
+			}
 
-        	// count Smileys
+			// A hashtag followed by whitespace is no hashtag in our meaning.
+			if (actChar == '#' && (unsigned char) *(1 + tweetString) != ' ')
+				thisData->hashtags++;
+		} else if (actChar < 224) { // 110x xxxx --> UTF8 Encoding beginnt 2Byte
+			actCharByteLenght = 2;
+			tweetString++;
+		} else if (actChar < 240) { // 1110 xxxx --> 3 Byte
+			actCharByteLenght = 3;
+			tweetString += 2;
+		} else {
+			actCharByteLenght = 4; // 1111 0xxx --> 4 Byte
+
+			// count Smileys
 			if (actChar == 240) {
-				actChar = (unsigned char)*(++tweetString); // next(tweetString);
+				actChar = (unsigned char) *(++tweetString); // next(tweetString);
 				if (actChar == 159) {
-					actChar = (unsigned char)*(++tweetString); // next(tweetString);
+					actChar = (unsigned char) *(++tweetString); // next(tweetString);
 					if (actChar == 152) {
-						actChar = (unsigned char)*(++tweetString); // next(tweetString);
+						actChar = (unsigned char) *(++tweetString); // next(tweetString);
 						if (actChar >= 129)
 							thisData->smiles++;
 					} else if (actChar == 153) {
-						actChar = (unsigned char)*(++tweetString); // next(tweetString);
+						actChar = (unsigned char) *(++tweetString); // next(tweetString);
 						if (actChar <= 143)
 							thisData->smiles++;
 					} else {
-						tweetString+=4-3;
+						tweetString += 4 - 3;
 					}
-				} else
-				{
-					tweetString+=4-2;
+				} else {
+					tweetString += 4 - 2;
 				}
-			} else
-			{
-				tweetString+=4-1;
+			} else {
+				tweetString += 4 - 1;
 			}
-        }
+		}
 
 		tweetString++;
 	}
 
 	return thisData;
-
 }
+
